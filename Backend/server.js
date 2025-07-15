079a577c738bb91e9ae0299e90a49d808a83282e
Hone_Tech <satyasri.kapisetti146@gmail.com/>
2025-07-15 22:38:55 +0530

    new version

--- Backend/server.js
+++ Backend/server.js
@@ -8,11 +8,16 @@ import errorHandler from "./utils/errorHandler.js";
 import flightroutes from "./routes/flightRoutes.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json({ limit: "10mb" }));
 app.use(
   cors({
